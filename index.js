129322bb36b8475923ed3d10f89ba5b39d2a5ab4
lecksa <dolabesipa@gmail.com>
2023-10-17 16:50:51 +0500

    message

---
+++ index.js
@@ -0,0 +1,24 @@
+let name = prompt('What is your name?')
+if(name.length % 2 === 0 ){
+    alert('Welcome!')
+} else{
+    alert('Go away!')
+}
+
+
+
+let number = prompt('Напишите любое число и вам покажут чётное оно или нет.')
+if(number % 2 === 0){
+    alert('Это чётное число.')
+} else{
+    alert('Это нечётное число.')
+}
+
+
+
+let home = prompt('What is your name?')
+if(home[0] == 'A' || home[0] == 'a'){
+    alert('Welcome!')
+} else{
+    alert('Go away!')
+}
